dcb9bed70f9410f9b9a991cd26c7cc1bb49a3218
Gutfreund, Yechezkal <ygutfreund@draper.com>
2019-11-01 11:47:24 -0400

    added populate

--- db.js
+++ db.js
@@ -1,12 +1,12 @@
 const Sequelize = require('sequelize');
 
-//const m3Db = new Sequelize('postgres://postgres:yechezkal@127.0.0.1:5432/m3_db')
+const { userData } = require('./userData')
 
-const m3Db = new Sequelize('m3_db', 'postgres', 'yechezkal',
+const DB = new Sequelize('m3_db', 'postgres', 'yechezkal',
     {
         dialect: 'postgres'
     })
-m3Db
+DB
     .authenticate()
     .then(() => {
         console.log('Connected to DB');
@@ -15,20 +15,30 @@ m3Db
         console.error('Unable to connect to DB', err);
     });
 
-const africom = m3Db.define('africom', {
-    cocom: { type: Sequelize.STRING },
-    descripton: { type: Sequelize.STRING },
-    cntry_name: { type: Sequelize.STRING }
-});
-
 async function findAllRows() {
 
-    let notes = await africom.findAll({ raw: true });
+    let notes = await user.findAll({ raw: true });
     console.log(notes);
 
     sequelize.close();
 }
 
-//findAllRows();
 
-module.exports.account = africom;
+async function populate() {
+    sequelize.sync({ force: true }).then(() => {
+        DB.bulkCreate(userData, { validate: true }).then(() => {
+            console.log('users created');
+        }).catch((err) => {
+            console.log('failed to create users');
+            console.log(err);
+        }).finally(() => {
+            sequelize.close();
+        });
+    });
+
+}
+
+module.exports = {
+    DB: DB,
+    populate: populate
+}
